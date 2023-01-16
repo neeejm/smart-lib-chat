296a53bc09745bbddb415c739d83af876b2274c6
Neeejm <nejmzakaria@gmail.com>
2023-01-16 22:52:25 +0100

    fix include bug

--- scripts/core.js
+++ scripts/core.js
@@ -37,7 +37,6 @@ export const answerQuestion = (question) => {
 
   // techonologies level
   if (technologiesKeywords.some((k) => keywords.includes((keyword = k)))) {
-    keywords = keywords.filter((el) => !technologiesKeywords.includes(el));
     let data = getTechData(keyword);
 
     // subject level
@@ -47,10 +46,11 @@ export const answerQuestion = (question) => {
 
     if (
       Object.keys(data).some((ks) =>
-        (keyword = ks).split(delimeter).some((k) => keywords.includes(k))
+        (keyword = ks).split(delimeter).some((k) => {
+          return keywords.includes(k);
+        })
       )
     ) {
-      keywords = keywords.filter((el) => !keyword.includes(el));
       data = data[keyword];
 
       // action level
